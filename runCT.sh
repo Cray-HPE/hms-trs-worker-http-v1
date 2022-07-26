0875eedb0e7017e0fc4bcd801514d550392ccd59
Shane Unruh <shane.unruh@hpe.com>
2022-07-26 17:06:01 -0600

    Added ct to workflow

--- runCT.sh
+++ runCT.sh
@@ -1,4 +1,9 @@
-# (C) Copyright [2021-2022] Hewlett Packard Enterprise Development LP
+#!/usr/bin/env bash
+
+#
+# MIT License
+#
+# (C) Copyright [2022] Hewlett Packard Enterprise Development LP
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -12,25 +17,47 @@
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
 # THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
 # OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
+#
+set -x
 
-# Service
-NAME ?= hms-trs-worker-http-v1
-VERSION ?= $(shell cat .version)
-DOCKER_IMAGE ?= ${NAME}:${VERSION}
 
-all: image snyk ct
+# Configure docker compose
+export COMPOSE_PROJECT_NAME=$RANDOM
+export COMPOSE_FILE=docker-compose.testing.yml
 
-image:
-	docker build --pull ${DOCKER_ARGS} --tag '${DOCKER_IMAGE}' .
+echo "COMPOSE_PROJECT_NAME: ${COMPOSE_PROJECT_NAME}"
+echo "COMPOSE_FILE: $COMPOSE_FILE"
 
-snyk:
-	./runSnyk.sh
 
-ct:
-	./runCT.sh
+function cleanup() {
+  docker-compose down
+  if ! [[ $? -eq 0 ]]; then
+    echo "Failed to decompose environment!"
+    exit 1
+  fi
+  exit $1
+}
 
+
+# Get the base containers running
+echo "Starting containers..."
+docker-compose build --no-cache
+docker-compose up  -d zookeeper kafka
+docker-compose up  -d worker
+
+docker-compose up --exit-code-from integration integration
+test_result=$?
+echo "Cleaning up containers..."
+if [[ $test_result -ne 0 ]]; then
+  echo "CT smoke tests FAILED!"
+  cleanup 1
+fi
+
+# Cleanup
+echo "CT tests PASSED!"
+cleanup 0
